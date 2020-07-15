65f40b3ac4585e160e7fb1e95315e06bbee2f1c6
Lyndsey Narvaez <lyndseypanini@gmail.com>
2020-07-15 13:52:43 -0700

    Initial Commit

---
+++ Green&Gleam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Green&Gleam
+//
+//  Created by Lyndsey Narvaez on 7/15/20.
+//  Copyright © 2020 Lyndsey Narvaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
