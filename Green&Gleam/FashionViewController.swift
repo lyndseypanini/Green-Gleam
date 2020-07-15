b7e109a58b69dc00ba3c14d5c49cdabbd60839b4
vivianyunwang <vivianyunwang@gmail.com>
2020-07-15 15:02:32 -0700

    Vivian's contributions 7-15-2020 (fashion page)

---
+++ Green&Gleam/FashionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FashionViewController.swift
+//  Green&Gleam
+//
+//  Created by Vivian Wang on 7/15/20.
+//  Copyright © 2020 Lyndsey Narvaez. All rights reserved.
+//
+
+import UIKit
+
+class FashionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
