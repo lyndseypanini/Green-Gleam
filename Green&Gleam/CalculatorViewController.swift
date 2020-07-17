f71f3b2abdfe87184079ff029e76507d555fc686
vikki <23vconroy@ndala.com>
2020-07-16 22:54:13 -0700

    added carbon footprint calculator

---
+++ Green&Gleam/CalculatorViewController.swift
@@ -0,0 +1,61 @@
+//
+//  CalculatorViewController.swift
+//  Green&Gleam
+//
+//  Created by Victoria Conroy on 7/16/20.
+//  Copyright © 2020 Lyndsey Narvaez. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    
+    var numberOnScreen: Double = 0;
+    var previousNumber: Double = 0;
+    var performingMath = false;
+    var operation = 0;
+    
+    @IBOutlet weak var labelTapped: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func numbersTapped(_ sender: UIButton) {
+        if performingMath == true {
+
+
+        performingMath = false
+
+        }
+
+        else {
+
+     
+        }
+    }
+}
+    
+        
+        
+       
+    
+
+    
+
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+
+
+
