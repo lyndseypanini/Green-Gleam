1e11e99739622f7c6059cc3ba33735423237ebb6
vivianyunwang <vivianyunwang@gmail.com>
2020-07-16 21:15:05 -0700

    Vivian 7/16/2020 9:15 PM - finished the app! Added a progress bar and some additional edits to finalize our features :)

--- Green&Gleam/ViewController.swift
+++ Green&Gleam/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Green&Gleam
 //
-//  Created by Lyndsey Narvaez on 7/15/20.
-//  Copyright © 2020 Lyndsey Narvaez. All rights reserved.
+//  Created by Lyndsey Narvaez, Vivian Wang, Eugean Choi, & Victoria Conroy on 7/15/20.
+//  Copyright © 2020 Lyndsey Narvaez, Vivian Wang, Eugean Choi, & Victoria Conroy. All rights reserved.
 //
 
 import UIKit
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var ChallengeTapped: UIToolbar!
     
     @IBAction func calculatorTapped(_ sender: UIButton) {
+    }
+    @IBAction func carbonFootprintCalcButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.apple.com")! as URL, options: [:], completionHandler: nil)
     }
 }
 
